63149a106b2aaaddb0d07259c8123c701a188588
francisco-guru <franciscojdiazs33@gmail.com>
2021-04-11 21:20:11 -0400

    Initial commit.

---
+++ AnalyzeResult.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Analyzer
+{
+	public class AnalyzeResult
+	{
+		public string Description
+		{
+			get;
+			set;
+		}
+
+		public string Expiry
+		{
+			get;
+			set;
+		}
+
+		public string Instrument
+		{
+			get;
+			set;
+		}
+
+		public decimal Price
+		{
+			get;
+			set;
+		}
+
+		public PriceTypes PriceType
+		{
+			get;
+			set;
+		}
+
+		public int PriceValue
+		{
+			get;
+			set;
+		}
+
+		public string Symbol
+		{
+			get;
+			set;
+		}
+
+		public AnalyzeResult()
+		{
+		}
+	}
+}
---
+++ StockQuote.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Analyzer
+{
+	public class StockQuote
+	{
+		public decimal Close
+		{
+			get;
+			set;
+		}
+
+		public string Description
+		{
+			get;
+			set;
+		}
+
+		public string Expiry
+		{
+			get;
+			set;
+		}
+
+		public decimal High
+		{
+			get;
+			set;
+		}
+
+		public string Instrument
+		{
+			get;
+			set;
+		}
+
+		public decimal Low
+		{
+			get;
+			set;
+		}
+
+		public decimal Open
+		{
+			get;
+			set;
+		}
+
+		public DateTime QuoteTime
+		{
+			get;
+			set;
+		}
+
+		public string Symbol
+		{
+			get;
+			set;
+		}
+
+		public int Volume
+		{
+			get;
+			set;
+		}
+
+		public StockQuote()
+		{
+		}
+	}
+}
